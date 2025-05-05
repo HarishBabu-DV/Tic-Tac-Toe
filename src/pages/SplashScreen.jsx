85b80b6cb7e4d782d197823077efbd99334640c9
HarishBabu-DV <harishbabudv2002@gmail.com>
2025-05-05 20:34:51 +0530

    modified layout of splashscreen for mobile

--- src/pages/SplashScreen.jsx
+++ src/pages/SplashScreen.jsx
@@ -5,14 +5,14 @@ const SplashScreen = () => {
   return (
     <section className='w-full h-screen py-10 flex flex-col gap-4 bg-gray-50'>
         {/* Tic Tac Toe Shape  */}
-        <div className='flex items-center justify-center gap-5 h-1/2 '>
+        <div className='xo-container '>
             <div className='x-shape size-[100px] '/> 
             <div className='o-shape size-[100px] '>
               <div className='size-[50px] rounded-full bg-gray-50'>
               </div>
             </div>
         </div>
-        <div className='h-1/2 flex flex-col gap-8 items-center'>
+        <div className='welcome-msg-container h-1/2 flex flex-col gap-8 items-center'>
           <h1 className='text-center text-2xl font-semibold'>Hello &#9995;</h1>
           <h1 className='text-center text-2xl font-semibold'>Welcome to Tic Tac Toe </h1>
             <Link className='play-button'>
