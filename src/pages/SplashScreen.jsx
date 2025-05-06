1a5ef80d9fa7d7f3747e35326d370559fe71e8b6
HarishBabu-DV <harishbabudv2002@gmail.com>
2025-05-06 11:42:56 +0530

    completed splashscreen responsively

--- src/pages/SplashScreen.jsx
+++ src/pages/SplashScreen.jsx
@@ -3,18 +3,18 @@ import { Link } from 'react-router'
 
 const SplashScreen = () => {
   return (
-    <section className='w-full h-screen py-10 flex flex-col gap-4 bg-gray-50'>
+    <section className='w-full h-screen py-10 flex flex-col gap-4 lg:flex-row bg-gray-50 lg:gap-0'>
         {/* Tic Tac Toe Shape  */}
         <div className='xo-container '>
-            <div className='x-shape size-[100px] '/> 
-            <div className='o-shape size-[100px] '>
-              <div className='size-[50px] rounded-full bg-gray-50'>
+            <div className='x-shape size-[100px] lg:size-[150px]'/> 
+            <div className='o-shape size-[100px] lg:size-[150px]'>
+              <div className='size-[50px] lg:size-[75px] rounded-full bg-gray-50'>
               </div>
             </div>
         </div>
-        <div className='welcome-msg-container h-1/2 flex flex-col gap-8 items-center'>
-          <h1 className='text-center text-2xl font-semibold'>Hello &#9995;</h1>
-          <h1 className='text-center text-2xl font-semibold'>Welcome to Tic Tac Toe </h1>
+        <div className='welcome-msg-container '>
+          <h1 className='text-center text-2xl font-semibold lg:text-4xl'>Hello &#9995;</h1>
+          <h1 className='text-center text-2xl font-semibold lg:text-4xl'>Welcome to Tic Tac Toe </h1>
             <Link className='play-button'>
                 Play
             </Link>
